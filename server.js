8e5dde8fc27208b557adb07b93d2089abfe9c566
Sanjay Kanwar <sanj.kan123@gmail.com>
2023-03-14 17:43:32 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,53 @@
-var express = require('express');
+import { NextFunction, Request, Response, RequestHandler } from 'express';
+import { decodeJwt } from 'jose';
+import { readFileSync } from 'fs';
 
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const configPath = () => {
+  return require('path').resolve(__dirname, './access-rules/access.json');
+};
 
+function hasAccess(keyCloakgroups: string[], apiPath: string): boolean {
+  const config = readFileSync(configPath(), { encoding: 'utf-8' });
+  const jsonData = JSON.parse(config);
 
-var	wine = require('./routes/wines');
+  const roleExists = Object.keys(jsonData.groups).filter((grp: string) => {
+    return keyCloakgroups.includes(grp);
+  });
 
-var app = express();
+  if (roleExists.length > 0) {
+    const apiAccess = Object.values(jsonData.groups).filter((vals: any) => {
+      console.log(vals.includes(apiPath), apiPath, vals);
+      return vals.includes(apiPath);
+    });
+    return apiAccess.length > 0;
+  } else {
+    return false;
+  }
+}
 
-app.get('wines/:id', wine.findById);
-
-
-
-app.listen(3000);
-console.log("Listening to port 3000...");
+export const authMiddleware: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    // /api/test/health
+    const token = req.headers['x-forwarded-access-token'] as string;
+    const apiPath = req.originalUrl.replace('/api', '');
+    if (!token) {
+      res.status(401).send('Unauthorized');
+      return;
+    } else {
+      const payload = decodeJwt(token);
+      const groups: string[] = payload?.groups;
+      if (hasAccess(groups, apiPath)) {
+        next();
+      } else {
+        res.status(401).send('Unauthorized');
+      }
+    }
+  } catch (error) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+};
