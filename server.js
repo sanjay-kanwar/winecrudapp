01e27d525149d3e9c4262b49eb9fccbbe0c95c4e
Sanjay Kanwar <sanj.kan123@gmail.com>
2023-03-14 17:46:00 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-{
-    "groups":{
-        "/list":[
-            "catalog",
-            "api",
-            "docs",
-            "api-docs",
-            "/test/health"
-        ],
-        "/bit-bucket-pr":[
-            "onboarding",
-            "routing-rules"
-        ],
-        "/security":[
-            "prometheus"
-        ],
-        "/resource-management":[
-            "kubernetes/workload"
-        ]
-    }
-}
+var express = require('express');
+
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var	wine = require('./routes/wines');
+
+var app = express();
+
+app.get('wines/:id', wine.findById);
+
+
+
+app.listen(3000);
+console.log("Listening to port 3000...");
